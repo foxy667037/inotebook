f279a75ed9f125140401f59b57e3f2f7b3f9566c
foxy667037 <ma667037h@gmail.com>
2024-04-01 20:22:21 +0500

    vedio 43

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.2";
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to mongo seccessfully");
+    mongoose.connect(mongoURI, () => {
+       return console.log("Connected to mongo seccessfully");
     })
 }
 
