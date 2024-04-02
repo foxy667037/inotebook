ca7027fc8d06cc50208183fdf9b33f1515de0112
foxy667037 <ma667037h@gmail.com>
2024-04-03 00:17:20 +0500

    vedio 46

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.2";
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.2";
 
 const connectToMongo = async() => {
     await
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 
 // Create a User using: POST "/api/auth/" . Doesn't require authentication
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/', [
+        body('name', 'Enter valid name').notEmpty().isLength({ min: 3}),
+        body('email', 'Enter valid email address').notEmpty().isEmail(),
+        body('password', 'Enter password minimum 5 characters').notEmpty().isLength({ min: 5})
+    ], 
+    (req, res) => {
+
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        res.send({ errors: result.array() });    
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    })
+    .then(user => res.json(user))
+    .catch(err => {
+        console.log(err)
+        res.json({error: 'Please Enter a unique value for email'})
+    });
+    
 })
 
 module.exports = router;
