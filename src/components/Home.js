16d8f6e26bd8d251e58419c0ded268ee47d4fd66
foxy667037 <ma667037h@gmail.com>
2024-04-20 23:13:20 +0500

    vedio 58

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useContext } from 'react';
+import { useEffect} from 'react';
 import noteContext from '../context/notes/noteContext';
 
 const Home = () => {
   
   const a = useContext(noteContext); 
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, [])
   
   return (
     <>
       <div className='container text-center my-5'>
         <h1>This is iNotebook - Home</h1>
         <h1>Inotebook</h1>
-        This is {a.name} and i am at class {a.class}
+        This is {a.state.name} and i am in class {a.state.class}
       </div>
     </>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import noteContext from './noteContext';
 
 const NoteState = (props) => {
 
-    const state = {
+    const s1 = {
         "name" : "FOXY",
         "class" : "12"
     }
 
+    const [ state , setState ] = useState(s1);
+    
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                "name" : "FAHAD",
+                "class" : "13"
+            })
+        }, 1000);
+    }
+
     return (
-        <noteContext.Provider value={state}>
+        <noteContext.Provider value={{state, update}}>
             {props.children}
         </noteContext.Provider>
     )
