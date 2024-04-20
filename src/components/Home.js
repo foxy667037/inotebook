377d1ea8283c5e86147c24eb8954a810e123af70
foxy667037 <ma667037h@gmail.com>
2024-04-21 00:01:35 +0500

    vedio 59

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { useContext } from 'react';
-import { useEffect} from 'react';
-import noteContext from '../context/notes/noteContext';
 
 const Home = () => {
   
-  const a = useContext(noteContext); 
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, [])
+ 
   
   return (
     <>
-      <div className='container text-center my-5'>
-        <h1>This is iNotebook - Home</h1>
-        <h1>Inotebook</h1>
-        This is {a.state.name} and i am in class {a.state.class}
+      <div className='container my-5'>
+        <h3>Add a note</h3>
+          <form>
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1"/>
+            </div>
+            <div className="mb-3 form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+              <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        <h3 className='my-4'>Your notes</h3>
       </div>
     </>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,26 +1,11 @@
-import { useState } from 'react';
 import noteContext from './noteContext';
 
 const NoteState = (props) => {
 
-    const s1 = {
-        "name" : "FOXY",
-        "class" : "12"
-    }
-
-    const [ state , setState ] = useState(s1);
-    
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name" : "FAHAD",
-                "class" : "13"
-            })
-        }, 1000);
-    }
+   
 
     return (
-        <noteContext.Provider value={{state, update}}>
+        <noteContext.Provider value={{}}>
             {props.children}
         </noteContext.Provider>
     )
